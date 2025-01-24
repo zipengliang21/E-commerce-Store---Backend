ef9303e669a8ae5f82a53ec165f2f77ad583ee8e
Zipeng Liang <zipengliang21@gmail.com>
2025-01-23 19:47:31 -0800

    Configure Django Admin

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from userauths.models import User, Profile
 
-admin.site.register(User)
-admin.site.register(Profile)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'gender', 'country']
+    # list_editable = ['gender', 'country']
+    search_fields = ['full_name']
+    list_filter = ['date']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
 
