426301410f9d37262c9044b7591dfe0aa3eadeb8
Zipeng Liang <zipengliang21@gmail.com>
2025-01-27 11:39:42 -0800

    Add Token Refresh Endpoint

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from userauths import views as userauths_views
 from store import views as store_views
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view())
 ]
