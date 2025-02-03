f20c726a8e65855f75968eb634e2c6336ce7bf30
Zipeng Liang <zipengliang21@gmail.com>
2025-02-02 18:12:21 -0800

    Start to implement password-reset function

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
     path('user/token/refresh/', TokenRefreshView.as_view()),
-    path('user/register/', userauths_views.RegisterView.as_view())
+    path('user/register/', userauths_views.RegisterView.as_view()),
+    path('user/password-reset/<email>/', userauths_views.PasswordResetEmailVerifyView.as_view())
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -5,7 +5,9 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from userauths.models import User, Profile
-from userauths.serializer import RegisterSerializer, MyTokenObtainPairSerializer
+from userauths.serializer import RegisterSerializer, MyTokenObtainPairSerializer, UserSerializer
+
+import shortuuid
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -14,3 +16,33 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+def generate_otp():
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:6]
+    return unique_key
+
+class PasswordResetEmailVerifyView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects.get(email=email)
+
+        if user:
+            user.otp = generate_otp()
+            user.save()
+
+            uidb64 = user.pk
+            otp = user.otp
+
+            link = f"http://localhost:5173/password-reset?otp={otp}&uidb64={uidb64}"
+
+            print("Link----", link)
+
+            # Send Email
+
+        return user
+
+    
