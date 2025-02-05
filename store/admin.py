3c0d36e9f4009b66d9c6be25c82df5815f563d2d
Zipeng Liang <zipengliang21@gmail.com>
2025-02-04 21:25:36 -0800

    Create Faqs, Reviews, Notifications, and Coupons models

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Product, Category, Gallery, Specification, Size, Color, Cart, CartOrder, CartOrderItem
+from store.models import Product, Category, Gallery, Specification, Size, Color, Cart, CartOrder, CartOrderItem, Review, Notification, Coupon
 
 class GalleryInline(admin.TabularInline):
   model = Gallery
@@ -24,10 +24,27 @@ class ProductAdmin(admin.ModelAdmin):
   search_fields = ["title"]
   inlines = [GalleryInline, SpecificationInline, SizeInline, ColorInline]
 
+class ProductReviewAdmin(admin.ModelAdmin):
+  list_editable = ['active']
+  list_editable = ['active']
+  list_display = ['user', 'product', 'review', 'reply' ,'rating', 'active']
+
+class NotificationAdmin(admin.ModelAdmin):
+  list_editable = ['seen']
+  list_display = ['order', 'seen', 'user', 'vendor', 'date']
+
+class CouponAdmin(admin.ModelAdmin):
+  list_editable = ['code', 'active', ]
+  list_display = ['vendor' ,'code', 'discount', 'active', 'date']
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 
 admin.site.register(Cart)
 admin.site.register(CartOrder)
 admin.site.register(CartOrderItem)
+
+admin.site.register(Review, ProductReviewAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(Coupon, CouponAdmin)
 
--- store/models.py
+++ store/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 from django.utils.text import slugify
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 from userauths.models import User, Profile
 from vendor.models import Vendor
 
 from shortuuid.django_fields import ShortUUIDField
+
+RATING = (
+  ( 1,  "★☆☆☆☆"),
+  ( 2,  "★★☆☆☆"),
+  ( 3,  "★★★☆☆"),
+  ( 4,  "★★★★☆"),
+  ( 5,  "★★★★★"),
+)
 
 class Category(models.Model):
   title = models.CharField(max_length=100)
@@ -22,11 +33,11 @@ class Category(models.Model):
 class Product(models.Model):
 
   STATUS = (
-      ("draft", "Draft"),
-      ("disabled", "Disabled"),
-      ("rejected", "Rejected"),
-      ("in_review", "In Review"),
-      ("published", "Published"),
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
   )
   title = models.CharField(max_length=100)
   image = models.FileField(upload_to='product', null=True, blank=True, default='product.jpg')
@@ -54,6 +65,14 @@ class Product(models.Model):
 
   def __str__(self):
     return self.title
+  
+  def product_rating(self):
+    product_rating = Review.objects.filter(product=self).aggregate(avg_rating=models.Avg("rating"))
+    return product_rating["avg_rating"]
+  
+  def save(self, *args, **kwargs):
+    self.rating = self.product_rating()
+    super(Product, self).save(*args, **kwargs)
   
 class Gallery(models.Model): 
   product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -111,74 +130,202 @@ class Cart(models.Model):
       return f'{self.cart_id} - {self.product.title}'
   
 class CartOrder(models.Model):
-    PAYMENT_STATUS = (
-        ("paid", "Paid"),
-        ("pending", "Pending"),
-        ("processing", "Processing"),
-        ("cancelled", "Cancelled"),
-    )
+  PAYMENT_STATUS = (
+      ("paid", "Paid"),
+      ("pending", "Pending"),
+      ("processing", "Processing"),
+      ("cancelled", "Cancelled"),
+  )
 
-    ORDER_STATUS = (
-        ("pending", "Pending"),
-        ("fulfilled", "Fulfilled"),
-        ("cancelled", "Cancelled"),
-    )
+  ORDER_STATUS = (
+      ("pending", "Pending"),
+      ("fulfilled", "Fulfilled"),
+      ("cancelled", "Cancelled"),
+  )
 
-    vendor = models.ManyToManyField(Vendor, blank=True)
-    buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+  vendor = models.ManyToManyField(Vendor, blank=True)
+  buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-    sub_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    shipping_amount = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    tax_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    service_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  sub_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  shipping_amount = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  tax_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  service_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
 
-    payment_status = models.CharField(choices=PAYMENT_STATUS, max_length=100, default="pending")
-    order_status = models.CharField(max_length=100, choices=ORDER_STATUS, default="pending")
+  payment_status = models.CharField(choices=PAYMENT_STATUS, max_length=100, default="pending")
+  order_status = models.CharField(max_length=100, choices=ORDER_STATUS, default="pending")
 
-    # Discounts
-    initial_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2, help_text="The original total before discounts")
-    saved = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, null=True, blank=True, help_text="Amount saved by customer")
+  # Discounts
+  initial_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2, help_text="The original total before discounts")
+  saved = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, null=True, blank=True, help_text="Amount saved by customer")
 
-    # Personal Informations
-    full_name = models.CharField(max_length=1000, null=True, blank=True)
-    email = models.CharField(max_length=1000, null=True, blank=True)
-    mobile = models.CharField(max_length=1000, null=True, blank=True)
+  # Personal Informations
+  full_name = models.CharField(max_length=1000, null=True, blank=True)
+  email = models.CharField(max_length=1000, null=True, blank=True)
+  mobile = models.CharField(max_length=1000, null=True, blank=True)
 
-    # Shipping Address
-    address = models.CharField(max_length=1000, null=True, blank=True)
-    city = models.CharField(max_length=1000, null=True, blank=True)
-    state = models.CharField(max_length=1000, null=True, blank=True)
-    country = models.CharField(max_length=1000, null=True, blank=True)
+  # Shipping Address
+  address = models.CharField(max_length=1000, null=True, blank=True)
+  city = models.CharField(max_length=1000, null=True, blank=True)
+  state = models.CharField(max_length=1000, null=True, blank=True)
+  country = models.CharField(max_length=1000, null=True, blank=True)
 
-    oid = ShortUUIDField(unique=True, length=10, alphabet="abcdefghijklmnopqrstuvxyz")
-    date = models.DateTimeField(auto_now_add=True)
+  oid = ShortUUIDField(unique=True, length=10, alphabet="abcdefghijklmnopqrstuvxyz")
+  date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-      return self.oid
+  def __str__(self):
+    return self.oid
     
 class CartOrderItem(models.Model):
-    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+  vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
-    qty = models.PositiveIntegerField(default=0)
-    price = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    sub_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    shipping_amount = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    service_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    tax_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    country = models.CharField(max_length=100, null=True, blank=True)
+  qty = models.PositiveIntegerField(default=0)
+  price = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  sub_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  shipping_amount = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  service_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  tax_fee = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  country = models.CharField(max_length=100, null=True, blank=True)
 
-    size = models.CharField(max_length=100, null=True, blank=True)
-    color = models.CharField(max_length=100, null=True, blank=True)
+  size = models.CharField(max_length=100, null=True, blank=True)
+  color = models.CharField(max_length=100, null=True, blank=True)
 
-    # Coupons
-    initial_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    saved = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
-    oid = ShortUUIDField(unique=True, length=10, alphabet="abcdefghijklmnopqrstuvxyz")
-    date = models.DateTimeField(auto_now_add=True)
+  # Coupons
+  initial_total = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  saved = models.DecimalField(default=0.00, max_digits=12, decimal_places=2)
+  oid = ShortUUIDField(unique=True, length=10, alphabet="abcdefghijklmnopqrstuvxyz")
+  date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-      return self.oid
+  def __str__(self):
+    return self.oid
+    
+class ProductFaq(models.Model):
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  email = models.EmailField(null=True, blank=True)
+  question = models.CharField(max_length=1000)
+  answer = models.TextField(null=True, blank=True)
+  active = models.BooleanField(default=False)
+  date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+      return self.question
+
+  class Meta:
+      verbose_name_plural = "Product FAQs"
+
+# Define a model for Reviews
+class Review(models.Model):
+  # A foreign key relationship to the User model with SET_NULL option, allowing null and blank values
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+  
+  # A foreign key relationship to the Product model with SET_NULL option, allowing null and blank values, and specifying a related name
+  product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True, related_name="reviews")
+  
+  # Text field for the review content
+  review = models.TextField()
+  
+  # Field for a reply with max length 1000, allowing null and blank values
+  reply = models.CharField(null=True, blank=True, max_length=1000)
+  
+  # Integer field for rating with predefined choices
+  rating = models.IntegerField(choices=RATING, default=None)
+  
+  # Boolean field for the active status
+  active = models.BooleanField(default=False)
+  
+  # Many-to-many relationships with User model for helpful and not helpful actions
+  helpful = models.ManyToManyField(User, blank=True, related_name="helpful")
+  not_helpful = models.ManyToManyField(User, blank=True, related_name="not_helpful")
+  
+  # Date and time field
+  date = models.DateTimeField(auto_now_add=True)
+
+  # Method to return a string representation of the object
+  def __str__(self):
+    if self.product:
+        return self.product.title
+    else:
+        return "Review"
+  
+  class Meta:
+    verbose_name_plural = "Reviews & Rating"
+    ordering = ["-date"]
+      
+  # Method to get the rating value
+  def get_rating(self):
+    return self.rating
+  
+  def profile(self):
+    return Profile.objects.get(user=self.user)
+
+# Signal handler to update the product rating when a review is saved
+@receiver(post_save, sender=Review)
+def update_product_rating(sender, instance, **kwargs):
+    if instance.product:
+        instance.product.save()
+
+# Define a model for Wishlist
+class Wishlist(models.Model):
+  # A foreign key relationship to the User model with CASCADE deletion
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+  # A foreign key relationship to the Product model with CASCADE deletion, specifying a related name
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="wishlist")
+  # Date and time field
+  date = models.DateTimeField(auto_now_add=True)
+  
+  class Meta:
+      verbose_name_plural = "Wishlist"
+  
+  # Method to return a string representation of the object
+  def __str__(self):
+      if self.product.title:
+          return self.product.title
+      else:
+          return "Wishlist"
+      
+# Define a model for Notification
+class Notification(models.Model):
+  # A foreign key relationship to the User model with CASCADE deletion
+  user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+  # A foreign key relationship to the Vendor model with CASCADE deletion
+  vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True, blank=True)
+  # A foreign key relationship to the CartOrder model with CASCADE deletion, specifying a related name
+  order = models.ForeignKey(CartOrder, on_delete=models.SET_NULL, null=True, blank=True)
+  # A foreign key relationship to the CartOrderItem model with CASCADE deletion, specifying a related name
+  order_item = models.ForeignKey(CartOrderItem, on_delete=models.SET_NULL, null=True, blank=True)
+  # Is read Boolean Field
+  seen = models.BooleanField(default=False)
+  # Date and time field
+  date = models.DateTimeField(auto_now_add=True)
+  
+  class Meta:
+      verbose_name_plural = "Notification"
+  
+  # Method to return a string representation of the object
+  def __str__(self):
+      if self.order:
+          return self.order.oid
+      else:
+          return "Notification"
+
+# Define a model for Coupon
+class Coupon(models.Model):
+  # A foreign key relationship to the Vendor model with SET_NULL option, allowing null values, and specifying a related name
+  vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True, related_name="coupon_vendor")
+  # Many-to-many relationship with User model for users who used the coupon
+  used_by = models.ManyToManyField(User, blank=True)
+  # Fields for code, type, discount, redemption, date, and more
+  code = models.CharField(max_length=1000)
+  # type = models.CharField(max_length=100, choices=DISCOUNT_TYPE, default="Percentage")
+  discount = models.IntegerField(default=1)
+  # redemption = models.IntegerField(default=0)
+  date = models.DateTimeField(auto_now_add=True)
+  active = models.BooleanField(default=True)
+  
+  # Method to return a string representation of the object
+  def __str__(self):
+      return self.code
