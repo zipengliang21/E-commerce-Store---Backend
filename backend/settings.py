7905d0f6e9df08b5874003f6179eaeece23f3023
Zipeng Liang <zipengliang21@gmail.com>
2025-03-10 15:05:36 -0700

    AWS S3 bucket deployment

--- backend/settings.py
+++ backend/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'anymail',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -170,6 +171,20 @@ ANYMAIL = {
 
 FROM_EMAIL = 'zipengliang21@gmail.com'
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+
+# AWS S3 Bucket Configurations
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_LOCATION = 'static'
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 
 SITE_URL = env("SITE_URL")
 
